5d7181cf332718bc19c45b1715f631340e1a7a87
Ulises-Diaz <a00837223@tec.mx>
2025-02-19 15:18:49 -0800

    lister_talker

---
+++ catkin_ws/build/basic_comms/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/ros1/catkin_ws/src/basic_comms
+# Build directory: /home/student/ros1/catkin_ws/build/basic_comms
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
