536f15f485cc1724598ff74e6f45e62c7d95b062
Ulises-Diaz <a00837223@tec.mx>
2025-02-22 20:30:37 -0800

    Markers2

--- catkin_ws/build/courseworks/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/ros1/catkin_ws/src/courseworks
-# Build directory: /home/student/ros1/catkin_ws/build/courseworks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
